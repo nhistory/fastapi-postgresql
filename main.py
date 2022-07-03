36808abf58aaaeabf11d836946b175e63c1235db
nhistory <nhistory@naver.com>
2022-07-03 18:05:13 -0300

    Create item schema with pydantic

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+  id:int
+  name:str
+  description:str
+  price:int
+  on_offer:bool
 
 @app.get('/')
 def index():
   return {"massage":"Hello World"}
+
+@app.get('/greet/{name}')
+def greet_name(name:str):
+  return {"greeting":f"Hello {name}"}
+
+@app.put('/item/{item_id}')
+def update_item(item_id:int, item:Item):
+  return {
+    'name':item.name,
+    'description':item.description,
+    'price':item.price,
+    'on_offer':item.on_offer
+    }
