474eb5402cfddb9652adb683645b83bb13b02255
nhistory <nhistory@naver.com>
2022-07-04 21:42:34 -0300

    Fix main.py for get all items method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+from database import SessionLocal
+import models
 
 app = FastAPI()
 
@@ -11,23 +13,51 @@ class Item(BaseModel):
   price:int
   on_offer:bool
 
-@app.get('/')
-def index():
-  return {"massage":"Hello World"}
+  class Config:
+    orm_mode = True
 
-@app.get('/greet/{name}')
-def greet_name(name:str):
-  return {"greeting":f"Hello {name}"}
+# # API for test
+# @app.get('/')
+# def index():
+#   return {"massage":"Hello World"}
 
-@app.get('/greet')
-def greet_optional_name(name:Optional[str]="user"):
-  return {"message":f"Hello {name}"}
+# @app.get('/greet/{name}')
+# def greet_name(name:str):
+#   return {"greeting":f"Hello {name}"}
+
+# @app.get('/greet')
+# def greet_optional_name(name:Optional[str]="user"):
+#   return {"message":f"Hello {name}"}
+
+# @app.put('/item/{item_id}')
+# def update_item(item_id:int, item:Item):
+#   return {
+#     'name':item.name,
+#     'description':item.description,
+#     'price':item.price,
+#     'on_offer':item.on_offer
+#     }
+
+db = SessionLocal()
+
+@app.get('/items', response_model=List[Item], status_code=200)
+def get_all_items():
+  items=db.query(models.Item).all()
+
+  return items
+
+@app.get('/item/{item_id}')
+def get_an_item(item_id:int):
+  pass
+
+@app.post('/items')
+def create_an_item():
+  pass
 
 @app.put('/item/{item_id}')
-def update_item(item_id:int, item:Item):
-  return {
-    'name':item.name,
-    'description':item.description,
-    'price':item.price,
-    'on_offer':item.on_offer
-    }
+def update_an_item(item_id:int):
+  pass
+
+@app.delete('/item/{item_id}')
+def delete_item(item_id:int):
+  pass
