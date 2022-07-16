c03a6c5d3dfdc57569de8df47f9f6d75970b6506
nhistory <nhistory@naver.com>
2022-07-15 23:21:50 -0300

    Create update method with item_id in API

--- main.py
+++ main.py
@@ -72,9 +72,17 @@ def create_an_item(item:Item):
 
   return new_item
 
-@app.put('/item/{item_id}')
-def update_an_item(item_id:int):
-  pass
+@app.put('/item/{item_id}', response_model=Item, status_code=status.HTTP_200_OK)
+def update_an_item(item_id:int,item:Item):
+  item_to_update=db.query(models.Item).filter(models.Item.id==item_id).first()
+  item_to_update.name=item.name
+  item_to_update.price=item.price
+  item_to_update.description=item.description
+  item_to_update.on_offer=item.on_offer
+
+  db.commit()
+
+  return item_to_update
 
 @app.delete('/item/{item_id}')
 def delete_item(item_id:int):
